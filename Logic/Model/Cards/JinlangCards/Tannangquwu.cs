58d1e7d1f3b6ca56392c8050f68798f1357906cf
darren4ten <darren4ten@163.com>
2020-12-10 14:14:05 +0800

    实现探囊取物的逻辑。

--- Logic/Model/Cards/JinlangCards/Tannangquwu.cs
+++ Logic/Model/Cards/JinlangCards/Tannangquwu.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Logic.Cards;
+using Logic.GameLevel;
+using Logic.GameLevel.Panel;
+using Logic.Model.Enums;
 
 namespace Logic.Model.Cards.JinlangCards
 {
@@ -16,14 +21,93 @@ namespace Logic.Model.Cards.JinlangCards
             this.DisplayName = "探囊取物";
         }
 
-        public override bool CanBePlayed()
-        {
-            return true;
-        }
-
         public override Task Popup()
         {
             throw new NotImplementedException();
+        }
+
+        protected override async Task<CardResponseContext> OnBeforePlayCard(CardRequestContext cardRequestContext, CardResponseContext cardResponseContext, RoundContext roundContext)
+        {
+            cardRequestContext.AttackType = Enums.AttackTypeEnum.Tannangquwu;
+            return await Task.FromResult(cardResponseContext);
+        }
+
+        protected override async Task<CardResponseContext> OnPlayCard(CardRequestContext cardRequestContext, CardResponseContext cardResponseContext, RoundContext roundContext)
+        {
+            return await ExecuteAction(cardRequestContext, cardResponseContext, roundContext);
+        }
+
+        /// <summary>
+        /// 具体的逻辑
+        /// </summary>
+        /// <param name="cardRequestContext"></param>
+        /// <param name="roundContext"></param>
+        /// <returns></returns>
+        protected async Task<CardResponseContext> ExecuteAction(CardRequestContext cardRequestContext, CardResponseContext cardResponseContext, RoundContext roundContext)
+        {
+            var wxResponse = await GroupRequestWuxiekeji(cardRequestContext, cardResponseContext, roundContext);
+            if (wxResponse.ResponseResult == Enums.ResponseResultEnum.Wuxiekeji)
+            {
+                wxResponse.ResponseResult = Enums.ResponseResultEnum.Success;
+                wxResponse.Message = "请求被无懈可击";
+                return wxResponse;
+            }
+            //
+            var target = cardRequestContext.TargetPlayers.First();
+            //获取target的所有牌，展示面板
+            var panel = new PanelBase
+            {
+                DisplayMessage = "请选择一张要抽取的牌",
+                EquipmentCards = PanelBase.ConvertToPanelCard(target.EquipmentSet, true),
+                InHandCards = PanelBase.ConvertToPanelCard(target.CardsInHand, false),//todo:混乱手牌顺序
+                MarkCards = PanelBase.ConvertToPanelCard(target.Marks, true),
+                CardOwner = target
+            };
+            var res = await PlayerContext.Player.ResponseCard(new CardRequestContext()
+            {
+                MaxCardCountToPlay = 1,
+                MinCardCountToPlay = 1,
+                SrcPlayer = target,
+                AttackType = AttackTypeEnum.SelectCard,
+                Message = panel.DisplayMessage,
+                Panel = panel
+            }, cardResponseContext, roundContext);
+            //移除Panel中SelectedBy为当前player的牌
+            await RemoveCardsFromPanel(panel, cardRequestContext, cardResponseContext, roundContext);
+            return res;
+        }
+
+        protected async Task RemoveCardsFromPanel(PanelBase panel, CardRequestContext request, CardResponseContext response, RoundContext roundContext)
+        {
+            //检查标记
+            var panelCards = panel.MarkCards?.Where(p => p.SelectedBy == PlayerContext.Player).ToList();
+            panelCards?.ForEach(async p =>
+            {
+                Console.WriteLine($"{PlayerContext.Player.PlayerId}的【{PlayerContext.Player.GetCurrentPlayerHero().Hero.DisplayName}】从{panel.CardOwner.PlayerId}的【{panel.CardOwner.GetCurrentPlayerHero().Hero.DisplayName}】抽取了{p.Card.DisplayName}");
+                await panel.CardOwner.RemoveMark(p.Mark);
+                //将此牌放置入手牌中
+                PlayerContext.Player.CardsInHand.Add(p.Card);
+            });
+            //检查装备
+            panelCards = panel.EquipmentCards?.Where(p => p.SelectedBy == PlayerContext.Player).ToList();
+            panelCards?.ForEach(async p =>
+            {
+                Console.WriteLine($"{PlayerContext.Player.PlayerId}的【{PlayerContext.Player.GetCurrentPlayerHero().Hero.DisplayName}】从{panel.CardOwner.PlayerId}的【{panel.CardOwner.GetCurrentPlayerHero().Hero.DisplayName}】抽取了{p.Card.DisplayName}");
+                await panel.CardOwner.RemoveEquipment(p.Card, request, response, roundContext);
+                panel.EquipmentCards.Remove(p);
+                //将此牌放置入手牌中
+                PlayerContext.Player.CardsInHand.Add(p.Card);
+            });
+            //检查手牌
+            panelCards = panel.InHandCards?.Where(p => p.SelectedBy == PlayerContext.Player).ToList();
+            panelCards?.ForEach(async p =>
+            {
+                Console.WriteLine($"{PlayerContext.Player.PlayerId}的【{PlayerContext.Player.GetCurrentPlayerHero().Hero.DisplayName}】从{panel.CardOwner.PlayerId}的【{panel.CardOwner.GetCurrentPlayerHero().Hero.DisplayName}】抽取了{p.Card.DisplayName}");
+                await panel.CardOwner.RemoveCardsInHand(new List<CardBase>() { p.Card }, request, response, roundContext);
+                //将此牌放置入手牌中
+                PlayerContext.Player.CardsInHand.Add(p.Card);
+            });
+            await Task.FromResult(0);
         }
     }
 }
---
+++ Tests/Card/TannangquwuTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Logic.ActionManger;
+using Logic.Enums;
+using Logic.GameLevel;
+using Logic.GameLevel.Levels;
+using Logic.Model.Cards.BaseCards;
+using Logic.Model.Cards.JinlangCards;
+using Logic.Model.Enums;
+using Logic.Model.Hero.Presizdent;
+using Logic.Model.Player;
+using Logic.Model.Skill;
+using Logic.Model.Skill.SubSkill;
+using NUnit.Framework;
+
+namespace Tests.Card
+{
+    [TestFixture]
+    public class TannangquwuTest
+    {
+        [Test]
+        public async Task TestTannangquwu_Success()
+        {
+            var gameLevel1 = new GameLevel1();
+            gameLevel1.OnLoad();
+            var qianghua1 = new Qianghua(5, 30);
+            var shatan1 = new Shatan(5, 50);
+            var star2Xiangyu = new PlayerHero(2, new Xiangyu(), null,
+                new List<SkillBase>(){
+                    shatan1
+                });
+            var star3Zhuyuanzhang = new PlayerHero(3, new Zhuyuanzhang(), null,
+                new List<SkillBase>(){
+                    new Qianghua(1,50),
+                    new Xixue(5,50),
+                });
+            var player1 = new Player(gameLevel1, new AiActionManager(), new List<PlayerHero>() { star2Xiangyu })
+            {
+                PlayerId = 1,
+                RoundContext = new RoundContext()
+                {
+                    AttackDynamicFactor = AttackDynamicFactor.GetDefaultDeltaAttackFactor()
+                }
+            };
+            var player2 = new Player(gameLevel1, new AiActionManager(), new List<PlayerHero>() { star3Zhuyuanzhang })
+            {
+                PlayerId = 2
+            };
+
+            player1.Init();
+            player2.Init();
+            var cardToPlay = new Tannangquwu().AttachPlayerContext(new PlayerContext() { Player = player1, GameLevel = gameLevel1 });
+            player1.CardsInHand.Add(cardToPlay);
+            player1.CardsInHand.Add(new Sha().AttachPlayerContext(new PlayerContext() { Player = player2, GameLevel = gameLevel1 }));
+            player2.CardsInHand.Add(new Sha().AttachPlayerContext(new PlayerContext() { Player = player2, GameLevel = gameLevel1 }));
+            player2.CardsInHand.Add(new Sha().AttachPlayerContext(new PlayerContext() { Player = player2, GameLevel = gameLevel1 }));
+
+
+            var response = await cardToPlay.PlayCard(new CardRequestContext()
+            {
+                TargetPlayers = new List<Player>()
+                    {
+                        player2
+                    }
+            }, player1.RoundContext);
+            Console.WriteLine($"Player1的手牌数：" + player1.CardsInHand.Count);
+            Assert.AreEqual(2, player1.CardsInHand.Count);
+            Assert.AreEqual(1, player2.CardsInHand.Count);
+        }
+    }
+}
