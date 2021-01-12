0940e2c8ed5bb0d9b217232eda744335964c4a63
darren4ten <darren4ten@163.com>
2021-01-12 15:10:15 +0800

    增加UI设计文档；增加英雄用户组件；

---
+++ UI/Levels/Level_001_NoFriend2Enemies.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UI.Levels
+{
+    /// <summary>
+    /// Interaction logic for Level_001_NoFriend2Enemies.xaml
+    /// </summary>
+    public partial class Level_001_NoFriend2Enemies : Window
+    {
+        public Level_001_NoFriend2Enemies()
+        {
+            InitializeComponent();
+        }
+    }
+}
