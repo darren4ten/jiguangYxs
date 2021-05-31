018954560f86f00c98115fe18682c8d5aa40bd6b
darren4ten <darren4ten@163.com>
2021-05-31 10:55:57 +0800

    重构卡牌点击事件处理逻辑到PlayerUIState

--- Logic/Model/Player/PlayerUIState.cs
+++ Logic/Model/Player/PlayerUIState.cs
@@ -6,6 +6,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Logic.Annotations;
+using Logic.Cards;
 using Logic.GameLevel;
 using Logic.GameLevel.Panel;
 using Logic.Model.Enums;
@@ -159,6 +160,95 @@ namespace Logic.Model.Player
             ActionBar.BtnAction3.IsVisible = false;
         }
 
+        public async void DefaultCardInHandClicked(CardBase card, Action<CardBase> uiAction)
+        {
+            //手牌被单击，则检查手牌是否可以打出
+            //检查最大可以弹出的手牌数 maxPopCardCount（默认1）
+            // maxPopCardCount=CardRequestContext!=null && CardRequestContext.MaxCardCountToPlay>1?CardRequestContext.MaxCardCountToPlay:1;
+            //检查当前已弹出的牌数是否超过了maxPopCardCount,如果是，则将最先弹出的卡牌收回；如果没有，则直接将该牌弹出
+            var recentRequest = BindPlayer.CardRequestContexts?.LastOrDefault();
+            var maxPopCardCount = recentRequest != null && recentRequest.MaxCardCountToPlay > 1 ? recentRequest.MaxCardCountToPlay : 1;
+            if (!card.IsPopout)
+            {
+                if (BindPlayer.CardsInHand.Count(c => c.IsPopout) >= maxPopCardCount)
+                {
+                    var firstCard = BindPlayer.CardsInHand.First(c => c.IsPopout);
+                    firstCard.IsPopout = false;
+                    uiAction(firstCard);
+                }
+                card.IsPopout = true;
+            }
+            else
+            {
+                card.IsPopout = false;
+            }
+            uiAction(card);
+            BindPlayer.PlayerUiState.RestoreSelectStatus(SelectStatusEnum.Idle);
+
+            var cards = BindPlayer.CardsInHand.Where(p => p.IsPopout).ToList();
+            //检查弹出的牌是否可以被打出，即检查弹出的牌是否是CardRequestContext需要的或者RoundContext的需求，
+            //  如果不符合，则ActionBar的DisplayMessage需要提示出牌不合理，只显示取消按钮
+            //  如果符合，则显示确认和取消按钮
+            //被动出牌
+            if (recentRequest != null)
+            {
+                if (recentRequest.IsMatch(cards))
+                {
+                    //继续检查出牌是否符合要求，如果符合要求
+                    //提示确认、取消
+                    BindPlayer.PlayerUiState.SetupOkCancelActionBar(recentRequest.RequestTaskCompletionSource, "确定出牌", "确定", "取消", (async () =>
+                    {
+                        recentRequest.RequestTaskCompletionSource.SetResult(new CardResponseContext()
+                        {
+                            Cards = cards,
+                            ResponseResult = ResponseResultEnum.Success
+                        });
+                        return await Task.FromResult(true);
+                    }));
+                }
+                else
+                {
+                    //提示还需要选择至少MaxCardCountToPlay-MinCardCountToPlay 张牌
+                    BindPlayer.PlayerUiState.SetupOkCancelActionBar(recentRequest.RequestTaskCompletionSource, "选择的牌不可以被打出", null, "取消");
+                }
+
+                await recentRequest.RequestTaskCompletionSource.Task;
+            }
+            //主动出牌
+            else
+            {
+                if (BindPlayer.IsInZhudongMode())
+                {
+                    var firstCard = cards.FirstOrDefault();
+                    if (firstCard != null)
+                    {
+                        if (firstCard.CanBePlayed())
+                        {
+                            var popRes = await firstCard.Popup();
+                        }
+                        else
+                        {
+                            //提示还需要选择至少MaxCardCountToPlay-MinCardCountToPlay 张牌
+                            BindPlayer.PlayerUiState.SetupOkCancelActionBar(BindPlayer.RoundContext?.RoundTaskCompletionSource, "选择的牌不可以被打出", null, "结束出牌");
+                        }
+
+                    }
+                    else
+                    {
+                        //提示还需要选择至少MaxCardCountToPlay-MinCardCountToPlay 张牌
+                        BindPlayer.PlayerUiState.SetupOkCancelActionBar(BindPlayer.RoundContext?.RoundTaskCompletionSource, "请出牌", null, "结束出牌");
+                    }
+                    if (BindPlayer.RoundContext?.RoundTaskCompletionSource != null)
+                    {
+                        await BindPlayer.RoundContext.RoundTaskCompletionSource.Task;
+                    }
+                }
+                else
+                {
+                    BindPlayer.PlayerUiState.SetupOkCancelActionBar(null, "选择的牌不可以被打出", null, null);
+                }
+            }
+        }
 
         /// <summary>
         /// 显示面板
